a947e7943fc2aec2d831b1dffce43069256bec61
cedric.farinazzo <cedric.farinazzo@epita.fr>
2019-04-11 23:21:15 +0200

    add cmakelists
    Signed-off-by: cedric.farinazzo <cedric.farinazzo@epita.fr>

---
+++ src/ANN/tools.c
@@ -0,0 +1,6 @@
+#ifndef _ANN_TOOLS_H_
+#define _ANN_TOOLS_H_
+
+
+
+#endif /* _ANN_TOOLS_H_ */
---
+++ src/Test/test.c
@@ -0,0 +1,13 @@
+/*
+ * test.c
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <criterion/criterion.h>
+
+Test(test, pass)
+{
+    cr_assert(1);
+}
