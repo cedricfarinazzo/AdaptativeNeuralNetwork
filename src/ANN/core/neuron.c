6b2ac7ff418ee3bdf9c2a89edcddfeccd78e9298
cedric.farinazzo <cedric.farinazzo@epita.fr>
2019-04-24 23:49:38 +0200

    feat(neuron): add struct and test
    Signed-off-by: cedric.farinazzo <cedric.farinazzo@epita.fr>

--- src/ANN/core/neuron.c
+++ src/ANN/core/neuron.c
@@ -1,2 +1,80 @@
+#include <stdlib.h>
+
 #include "neuron.h"
 
+
+struct NEURON *NEURON_new(size_t size, double(*f_init)(), double(*f_act)(double))
+{
+    if (size == 0 || f_init == NULL)
+        return NULL;
+    struct NEURON *n = malloc(sizeof(struct NEURON));
+    if (n == NULL)
+        return NULL;
+    n->weights = malloc(sizeof(double) * size);
+    if (n->weights == NULL)
+    { free(n); return NULL; }
+    n->size = size; n->output = n->activation = n->delta = 0;
+    n->bias = f_init();
+    for (size_t i = 0; i < size; ++i)
+        n->weights[i] = f_init();
+    n->f_init = f_init;
+    n->f_act = f_act;
+    return n;
+}
+
+
+void NEURON_clear(struct NEURON *n)
+{
+    if (n != NULL)
+        n->output = n->activation = n->delta = 0;
+}
+
+
+void NEURON_free(struct NEURON *n)
+{
+    if (n != NULL)
+    {
+        free(n->weights);
+        free(n);
+    }
+}
+
+
+double NEURON_feedforward(struct NEURON *n, double *inputs, double(*f_act)(double))
+{
+    if (n == NULL || inputs == NULL)
+        return 0;
+    if (f_act != NULL)
+        n->f_act = f_act;
+    if (n->f_act == NULL)
+        return 0;
+    n->activation = n->bias;
+    for (size_t i = 0; i < n->size; ++i)
+        n->activation += n->weights[i] * inputs[i];
+    n->output = n->f_act(n->activation);
+    return n->output;
+}
+
+
+struct NEURON *NEURON_clone_stat(struct NEURON *n)
+{
+    if (n == NULL) return NULL;
+    return NEURON_new(n->size, n->f_init, n->f_act);
+}
+
+
+struct NEURON *NEURON_clone_all(struct NEURON *n)
+{
+    if (n == NULL) return NULL;
+    struct NEURON *b = malloc(sizeof(struct NEURON));
+    if (b == NULL)
+        return NULL;
+    b->weights = malloc(sizeof(double) * n->size);
+    if (b->weights == NULL)
+    { free(b); return NULL; }
+    b->bias = n->bias; b->output = n->output; b->activation = n->activation;
+    b->delta = n->delta; b->f_init = n->f_init; b->f_act = n->f_act; b->size = n->size;
+    for(size_t i = 0; i < b->size; ++i)
+        b->weights[i] = n->weights[i];
+    return b;
+}
---
+++ src/Test/test_core_neuron.c
@@ -0,0 +1,119 @@
+/*
+ * test_core_neuron.c
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+
+#include <criterion/criterion.h>
+
+#include "../ANN/core/neuron.h"
+
+void setup() {
+    srand(time(NULL));
+}
+
+double f_init()
+{
+    return (((double)rand())/RAND_MAX*2.0-1.0)/2.0;
+}
+
+double f_act(double n)
+{
+    return 1/(1+exp(-n));
+}
+
+Test(NEURON, Init)
+{
+    size_t s = 2;
+    struct NEURON *n = NEURON_new(s, f_init, f_act);
+    cr_expect_not_null(n);
+    cr_expect_eq(n->size, s);
+    cr_expect_eq(n->output, 0);
+    cr_expect_eq(n->activation, 0);
+    cr_expect_eq(n->delta, 0);
+    
+    NEURON_free(n);
+}
+
+Test(NEURON, WrongInit)
+{
+    size_t s = 0;
+    struct NEURON *n = NEURON_new(s, f_init, NULL);
+    cr_expect_null(n);
+    NEURON_free(n);
+}
+
+Test(NEURON, feedforward)
+{
+    size_t s = 2;
+    struct NEURON *n = NEURON_new(s, f_init, NULL);
+    
+    double inputs[] = {2.5, 3.14159};
+    cr_expect_neq(NEURON_feedforward(n, inputs, f_act), 0);
+    NEURON_clear(n);
+    cr_expect_eq(n->output, 0);
+    cr_expect_eq(n->activation, 0);
+    cr_expect_eq(n->delta, 0);
+
+    NEURON_free(n);
+}
+
+Test(NEURON, feedforward2)
+{
+    size_t s = 2;
+    struct NEURON *n = NEURON_new(s, f_init, f_act);
+    
+    double inputs[] = {2.5, 3.14159};
+    cr_expect_neq(NEURON_feedforward(n, inputs, NULL), 0);
+    NEURON_clear(n);
+    cr_expect_eq(n->output, 0);
+    cr_expect_eq(n->activation, 0);
+    cr_expect_eq(n->delta, 0);
+
+    NEURON_free(n);
+}
+
+Test(NEURON, Clone_stat)
+{
+    size_t s = 3;
+    struct NEURON *n = NEURON_new(s, f_init, NULL);
+    
+    struct NEURON *b = NEURON_clone_stat(n);
+    cr_expect_not_null(b);
+    cr_expect_eq(b->size, n->size);
+    cr_expect_eq(b->f_init, n->f_init);
+    cr_expect_eq(b->f_act, n->f_act);
+    
+    cr_expect_neq(b->bias, n->bias);
+    
+    NEURON_free(n);
+    NEURON_free(b);
+}
+
+Test(NEURON, Clone_all)
+{
+    size_t s = 42;
+    struct NEURON *n = NEURON_new(s, f_init, f_act);
+    
+    struct NEURON *b = NEURON_clone_all(n);
+    cr_expect_not_null(b);
+    cr_expect_eq(b->size, n->size);
+    cr_expect_eq(b->f_init, n->f_init);
+    cr_expect_eq(b->f_act, n->f_act);
+    
+    cr_expect_eq(b->bias, n->bias);
+    cr_expect_eq(b->output, 0);
+    cr_expect_eq(b->activation, 0);
+    cr_expect_eq(b->delta, 0);
+    
+    for(size_t i = 0; i < n->size; ++i)
+        cr_expect_eq(b->weights[i], n->weights[i]);
+    
+    NEURON_free(n);
+    NEURON_free(b);
+}
+
