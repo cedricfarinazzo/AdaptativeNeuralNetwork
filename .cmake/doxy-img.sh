aadab376638a0ee7bf6089f7066495a553e6d850
cedric.farinazzo <cedric.farinazzo@epita.fr>
2019-05-07 22:56:08 +0200

    doxygen: fix html
    Signed-off-by: cedric.farinazzo <cedric.farinazzo@epita.fr>

--- .cmake/doxy-img.sh
+++ .cmake/doxy-img.sh
@@ -3,7 +3,7 @@
 main () {
     html_file="$1"
     echo "Update svg image link in ${html_file}"
-    perl -pe 's/<object.*data="(.+)".*alt="(.*)">/<img src="$1" alt="$2" \/>/gsm' -i "${html_file}"
+    perl -pe 's/<object.*data="(.+)".*alt="(.+)"><\/object>/<img src="$1" alt="$2"/gsm' -i "${html_file}"
 }
 
 main "$@"
